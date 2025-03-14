22cb49ec5adf81b35843e4452ada85b20f37eac5
LearnWithSuryaa <davindra514@gmail.com>
2025-03-14 23:34:42 +0700

    Update Modul_2

--- Codelab/Modul_2/src/Codelab_1.java
+++ Codelab/Modul_2/src/Codelab_1.java
@@ -1,27 +1,57 @@
 class Hewan {
-    String nama;
-    String jenis;
-    String suara;
+    // Atribut private untuk enkapsulasi
+    private String nama;  
+    private String jenis;
+    private String suara;
 
-    Hewan(String nama, String jenis, String suara) {
+    // Konstruktor
+    public Hewan(String nama, String jenis, String suara) {
         this.nama = nama;
         this.jenis = jenis;
         this.suara = suara;
     }
 
-    void tampilkanInfo() {
-        System.out.println("Nama: " + nama);
-        System.out.println("Jenis: " + jenis);
-        System.out.println("Suara: " + suara + "\n");
+    // Getter untuk membaca data
+    public String getNama() {
+        return nama;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public String getSuara() {
+        return suara;
+    }
+
+    // Override metode toString untuk representasi objek
+    @Override
+    public String toString() {
+        return "Nama: " + nama + "\nJenis: " + jenis + "\nSuara: " + suara + "\n";
     }
 }
 
 public class Codelab_1 {
     public static void main(String[] args) {
+        // Membuat objek hewan
         Hewan hewan1 = new Hewan("Kucing", "Mamalia", "Nyann~~");
         Hewan hewan2 = new Hewan("Anjing", "Mamalia", "Woof-Woof!!");
 
-        hewan1.tampilkanInfo();
-        hewan2.tampilkanInfo();
+        // Menampilkan informasi hewan dengan toString()
+        System.out.println(hewan1);
+        System.out.println(hewan2);
     }
 }
+
+/*
+Note:
+1. Saya menggunakan @Override pada toString()  
+   - toString() adalah metode bawaan dari class Object.  
+   - Dengan override, saya bisa menentukan bagaimana objek Hewan direpresentasikan sebagai String.  
+   - Dengan ini saya dapat mempermudah pencetakan objek tanpa harus membuat metode khusus seperti tampilkanInfo().  
+
+2. Saya juga menggunakan Enkapsulasi (private + getter)  
+   - Atribut dibuat private untuk melindungi data agar tidak bisa diubah langsung dari luar class.  
+   - Hanya bisa diakses dengan metode getter (getNama(), getJenis(), getSuara()), mengimplementasikan prinsip OOP yang dijelaskan pada saat praktikum.  
+   - Mempermudah jika perlu mengubah nilai, dengan cara menambahkan setter untuk memberikan kontrol lebih lanjut.  
+*/
--- Codelab/Modul_2/src/Codelab_2.java
+++ Codelab/Modul_2/src/Codelab_2.java
@@ -1,52 +1,70 @@
-class RekeningBank {
-    String nomorRekening;
-    String namaPemilik;
-    double saldo;
+import java.text.DecimalFormat;
 
-    // Konstruktor untuk menginisialisasi atribut
-    RekeningBank(String nomorRekening, String namaPemilik, double saldo) {
+class RekeningBank {
+    private String nomorRekening;
+    private String namaPemilik;
+    private double saldo;
+
+    // Formatter untuk menampilkan saldo dalam format uang
+    private static final DecimalFormat df = new DecimalFormat("#,##0.00");
+
+    // Konstruktor
+    public RekeningBank(String nomorRekening, String namaPemilik, double saldo) {
         this.nomorRekening = nomorRekening;
         this.namaPemilik = namaPemilik;
         this.saldo = saldo;
     }
 
+    // Getter untuk membaca atribut
+    public String getNomorRekening() {
+        return nomorRekening;
+    }
+
+    public String getNamaPemilik() {
+        return namaPemilik;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
     // Method untuk menampilkan informasi rekening
-    void tampilkanInfo() {
+    public void tampilkanInfo() {
         System.out.println("Nomor Rekening: " + nomorRekening);
         System.out.println("Nama Pemilik: " + namaPemilik);
-        System.out.println("Saldo: Rp" + saldo + "\n");
+        System.out.println("Saldo: Rp" + df.format(saldo) + "\n");
     }
 
     // Method untuk menyetor uang
-    void setorUang(double jumlah) {
+    public void setorUang(double jumlah) {
         if (jumlah > 0) {
             saldo += jumlah;
-            System.out.println(namaPemilik + " menyetor Rp" + jumlah + ". Saldo sekarang: Rp" + saldo + "\n");
+            System.out.println(namaPemilik + " menyetor Rp" + df.format(jumlah) + ". Saldo sekarang: Rp" + df.format(saldo) + "\n");
         } else {
-            System.out.println("Jumlah setoran harus lebih dari 0!" + "\n");
+            System.out.println("Jumlah setoran harus lebih dari 0!\n");
         }
     }
 
     // Method untuk menarik uang
-    void tarikUang(double jumlah) {
+    public void tarikUang(double jumlah) {
         if (jumlah > 0 && jumlah <= saldo) {
             saldo -= jumlah;
-            System.out.println(namaPemilik + " menarik Rp" + jumlah + ". (Berhasil) Saldo sekarang: Rp" + saldo + "\n");
+            System.out.println(namaPemilik + " menarik Rp" + df.format(jumlah) + ". (Berhasil) Saldo sekarang: Rp" + df.format(saldo) + "\n");
         } else if (jumlah > saldo) {
-            System.out.println(namaPemilik + " menarik Rp" + jumlah
-                    + ". (Gagal, Saldo tidak mencukupi) Saldo saat ini: Rp" + saldo + "\n");
+            System.out.println(namaPemilik + " menarik Rp" + df.format(jumlah)
+                    + ". (Gagal, Saldo tidak mencukupi) Saldo saat ini: Rp" + df.format(saldo) + "\n");
         } else {
-            System.out.println("Jumlah penarikan harus lebih dari 0!" + "\n");
+            System.out.println("Jumlah penarikan harus lebih dari 0!\n");
         }
     }
 }
 
 public class Codelab_2 {
     public static void main(String[] args) {
-        // Membuat dua objek RekeningBank
+        // Menggunakan nama pemilik asli
         RekeningBank rekening1 = new RekeningBank("202410370110235", "Surya", 2000000.0);
         RekeningBank rekening2 = new RekeningBank("202410370110246", "Chicka", 1000000.0);
-        
+
         // Menampilkan informasi awal rekening
         rekening1.tampilkanInfo();
         rekening2.tampilkanInfo();
@@ -64,3 +82,17 @@ public class Codelab_2 {
         rekening2.tampilkanInfo();
     }
 }
+
+/*
+Catatan:
+1. Sama seperti sebelumnya saya menggunakan Enkapsulasi (private + getter)  
+   - pada atribut nomorRekening, namaPemilik, dan saldo saya buat private untuk mencegah perubahan langsung dari luar class.  
+   - Ini menjaga integritas data dan memastikan perubahan hanya bisa dilakukan melalui metode yang telah ditentukan.  
+
+2. Saya juga menggunakan DecimalFormat untuk Saldo  
+   - Untuk memastikan saldo ditampilkan dalam format uang yang lebih mudah dibaca, misalnya Rp2,000,000.00 dibanding 2000000.0.  
+
+3. Menambahkan validasi pada Metode setorUang() dan tarikUang()  
+   - pada metode setorUang() saya menambahkan validasi inputan hanya menerima jumlah positif.  
+   - pada metide tarikUang() saya memastikan saldo cukup sebelum melakukan penarikan, mencegah saldo menjadi negatif.
+*/
